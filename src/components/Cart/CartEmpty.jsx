834f96cf72889f38ca9c5d6934312d506dbaeb27
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-02-27 12:10:12 +0530

    changed the favicon in the main page

--- src/components/Cart/CartEmpty.jsx
+++ src/components/Cart/CartEmpty.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import React from "react";
+import emptybag from '../../assets/emptybag.png';
 
 const CartEmpty = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="flex items-center justify-center flex-col h-screen px-11 text-center gap-7">
+        <img src={emptybag} alt="emptybag/img"  className="w-40 lg:w-36 sm:w-28 h-auto object-fill transition-all duration-300 hover:scale-110 cursor-pointer"/>
+        <button type="button" className="button-theme bg-gradient-to-b from-amber-500 to-orange-500 shadow-lg shadow-orange-500 flex items-center justify-center text-slate-900 py-2 gap-3 text-sm px-5 font-semibold hover:scale-110 duration-300">
+          <ArrowLeftIcon className="w-5 h-5 text-slate-900" />
+          <span className="">Back To Nike Store</span>
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default CartEmpty
+export default CartEmpty;
